46f3a1b6ac01caa0efc76fcbce3250448ec23308
d1manpro <dp.devops@yandex.ru>
2026-05-07 12:16:32 +0400

    feat(cli): add lyrics field to get command

--- tags/get.go
+++ tags/get.go
@@ -1,11 +1,15 @@
 package tags
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/bogem/id3v2/v2"
+	"github.com/d1manpro/mustag/ui"
 )
+
+var ManyLyricsErr = errors.New("file have many lyrics flags")
 
 type Field struct {
 	Key   string
@@ -24,6 +28,13 @@ func GetField(tag *id3v2.Tag, field string) (string, bool) {
 		return tag.Year(), true
 	case "genre":
 		return tag.Genre(), true
+	case "lyrics":
+		lyr, err := GetLyrics(tag)
+		if err != nil && !errors.Is(err, ManyLyricsErr) {
+			ui.Error(err.Error())
+			return "", false
+		}
+		return lyr, true
 	default:
 		return "", false
 	}
@@ -70,4 +81,27 @@ func GetAll(tag *id3v2.Tag) []Field {
 	}
 
 	return out
+}
+
+func GetLyrics(tag *id3v2.Tag) (string, error) {
+	if frames := tag.GetFrames("USLT"); len(frames) > 0 {
+		if len(frames) == 1 {
+			if lyr, ok := frames[0].(id3v2.UnsynchronisedLyricsFrame); ok {
+				return lyr.Lyrics, nil
+			} else {
+				return "", fmt.Errorf("lyrics parse error")
+			}
+		} else {
+			var lyrics []string
+			for _, f := range frames {
+				if lyr, ok := f.(id3v2.UnsynchronisedLyricsFrame); ok {
+					lyrics = append(lyrics, fmt.Sprintf("=== [Description: %s, Lang: %s, Size: %d] ===\n%s", lyr.ContentDescriptor, lyr.Language, lyr.Size(), lyr.Lyrics))
+				} else {
+					return "", fmt.Errorf("lyrics parse error")
+				}
+			}
+			return strings.Join(lyrics, "\n\n\n"), ManyLyricsErr
+		}
+	}
+	return "", nil
 }
